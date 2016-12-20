f5d78c2b5a60a81b3051350d192d07eed20e0d72
한정현(Junghyun Han)/Syrup Store개발팀/SKP <junghyun.han@sk.com>
2016-12-20 22:01:26 +0900

    api  연동

--- resources/js/search.js
+++ resources/js/search.js
@@ -2,23 +2,37 @@
 
 
 
-    angular.module( "iju" ).controller( "ijuCtrl", [ "$scope", "$http", function( $scope, $http ) {
+    angular.module( "iju" ).controller( "ijuCtrl", [ "$scope", "$http", "$httpParamSerializer", function( $scope, $http, $httpParamSerializer ) {
+
+        function makeQuery( modelList ){
+            var queryStringArray = [];
+            if ( !Array.isArray( modelList ) ) {
+                return;
+            }
+            modelList.forEach( function( v ) {
+                if ( typeof $scope[ v ] !== "undefined" ) {
+                    if ( v === "option" ) {
+                        queryStringArray.push( $scope[ v ].replace( /\ +/g, "+" ) );
+                    } else {
+                        queryStringArray.push( $scope[ v ] );
+                    }
+                }
+            } );
+            return queryStringArray.join( "+" );
+        }
 
         function search(){
-            var req = {
-                method: 'POST',
-                url: 'https://openapi.naver.com/v1/search/shop.json',
-                headers: {
-                    "X-Naver-Client-Id": "dJdCUoFXDK_PXuGff36e",
-                    "X-Naver-Client-Secret": "r8RuqNlwCA"
-                },
-                data: {
-                    query: encodeURIComponent( $scope.searchWord ),
-                    sort: "sim",
-                    display: "100"
-                }
+            var param = {
+                sort: "sim",
+                display: "100"
             };
-            $http( req ).then( function( res ) {
+            if ( !$scope.maker || !$scope.model ) {
+                alert( "제조사와 제품명은 꼭 입력해주세요." );
+                return;
+            }
+            param.query = makeQuery( [ "maker", "model", "version", "option" ] );
+
+            $http.get( 'http://139.162.71.151:3000/api/search/shop?' + $httpParamSerializer( param )  ).then( function( res ) {
                 console.log( res.data );
             }, function( res ) {
                 alert( "실패" );
@@ -32,11 +46,11 @@
 
 
 
-        $(document).on("click", ".btn-success", function( e ){
-            e.preventDefault();
-            alert("입력하신 키워드가 너무 다양한 검색 결과를 포함하고 있습니다.\n검색 결과를 좁히기 위해 구체적인 상품 이름을 입력해 주세요.\n예)\n스마트폰(X),\nApple iPhone 7 128GB(O)");
-            $("#search-result").show();
-        });
+        //$(document).on("click", ".btn-success", function( e ){
+        //    e.preventDefault();
+        //    alert("입력하신 키워드가 너무 다양한 검색 결과를 포함하고 있습니다.\n검색 결과를 좁히기 위해 구체적인 상품 이름을 입력해 주세요.\n예)\n스마트폰(X),\nApple iPhone 7 128GB(O)");
+        //    $("#search-result").show();
+        //});
 
 
     } ] );
